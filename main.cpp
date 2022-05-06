15797d09a484397e0131be6aa022e96141685006
ankbhatia19 <ankbhatia19@gmail.com>
2022-05-05 20:13:35 -0700

    Added micro-ros functionality with mbed6.8, includes publisher and subscriber

---
+++ main.cpp
@@ -0,0 +1,104 @@
+#include "mbed.h"
+
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <std_msgs/msg/int32.h>
+
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <rmw_microros/rmw_microros.h>
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);while(1){};}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+
+
+// --- micro-ROS Transports ---
+extern "C" bool mbed_serial_open(struct uxrCustomTransport * transport);
+extern "C" bool mbed_serial_close(struct uxrCustomTransport * transport);
+extern "C" size_t mbed_serial_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err);
+extern "C" size_t mbed_serial_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err);
+
+// --- micro-ROS App ---
+rcl_publisher_t publisher;
+rcl_subscription_t subscriber;
+std_msgs__msg__Int32 send_msg;
+std_msgs__msg__Int32 recv_msg;
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+	(void) last_call_time;
+	if (timer != NULL) {
+		RCSOFTCHECK(rcl_publish(&publisher, &send_msg, NULL));
+	}
+}
+
+void subscription_callback(const void * msgin)
+{
+	const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+    send_msg.data = msg->data;
+}
+
+int main()
+{
+    //printf("hello world\n");
+    DigitalOut led(LED2);
+
+	rmw_uros_set_custom_transport(
+        true,
+        NULL,
+        mbed_serial_open,
+        mbed_serial_close,
+        mbed_serial_write,
+        mbed_serial_read
+    );
+
+	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
+
+	// create init_options
+	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+	// create node
+	rcl_node_t node;
+	RCCHECK(rclc_node_init_default(&node, "nucleo_f446re", "", &support));
+
+	// create publisher
+	RCCHECK(rclc_publisher_init_default(
+		&publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"gimbal_data_echo"));
+
+  	// create subscriber
+	RCCHECK(rclc_subscription_init_default(
+		&subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"gimbal_data"));
+
+	// create timer,
+	rcl_timer_t timer;
+	const unsigned int timer_timeout = 1000;
+	RCCHECK(rclc_timer_init_default(
+		&timer,
+		&support,
+		RCL_MS_TO_NS(timer_timeout),
+		timer_callback));
+
+	// create executor
+	rclc_executor_t executor;
+	RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+	RCCHECK(rclc_executor_add_timer(&executor, &timer));
+	RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &recv_msg, &subscription_callback, ON_NEW_DATA));
+
+	send_msg.data = 0;
+	
+  	while(1){
+		rclc_executor_spin(&executor);
+	}
+
+	RCCHECK(rcl_subscription_fini(&subscriber, &node));
+	RCCHECK(rcl_publisher_fini(&publisher, &node));
+	RCCHECK(rcl_node_fini(&node));
+}
