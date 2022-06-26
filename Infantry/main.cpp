e9a76c159339d87fcf8adb0aef6104ec4653b777
Dexin Zhou <zhoudexin00@126.com>
2022-06-26 00:35:39 -0500

    Squashed commit of the following:
    commit 80e366dadd6612d507024ad4a2912ddbfc074f37
    Merge: 369471a c248414
    Author: Mew463 <mwyeoh123@gmail.com>
    Date:   Sat Jun 25 21:22:43 2022 -0500
    
        Merge branch 'main' of https://github.com/Triton-Robotics/TR-mbed6
    
    commit 369471abf49cf237b19d80a8cf0f77b2b4abcdea
    Author: Mew463 <mwyeoh123@gmail.com>
    Date:   Sat Jun 25 21:22:30 2022 -0500
    
        stuff
    
    commit c248414f994e938c08430183c757dd9766363a16
    Merge: e5419b2 ac90b77
    Author: pknessness <93059296+pknessness@users.noreply.github.com>
    Date:   Sat Jun 25 19:37:25 2022 -0500
    
        Merge branch 'main' of https://github.com/Triton-Robotics/TR-mbed6
    
    commit e5419b2f19d7aed7921a0c690897275c184ceab5
    Author: pknessness <93059296+pknessness@users.noreply.github.com>
    Date:   Sat Jun 25 19:37:15 2022 -0500
    
        new untested hero POWER code
    
    commit 05fcccfd9c6437f461da1f3bc236180d7beb7d23
    Author: pknessness <93059296+pknessness@users.noreply.github.com>
    Date:   Sat Jun 25 19:10:55 2022 -0500
    
        back to joystick shit
    
    commit ac90b7758d0bec53d4d8839b9bc521315265cd23
    Author: Mew463 <mwyeoh123@gmail.com>
    Date:   Sat Jun 25 17:47:33 2022 -0500
    
        sentry auto strafe, infantry updates
    
    commit 3398feaf285f4df807c8318b8471df1bc10ac847
    Author: pknessness <93059296+pknessness@users.noreply.github.com>
    Date:   Sat Jun 25 14:32:01 2022 -0500
    
        Stufff, hero works, pid tuner mods for ming, canmotor increased gimbly pid outcap
    
    commit d8826e070b5f53dfbafcaae755d3aee501447ccf
    Author: pknessness <93059296+pknessness@users.noreply.github.com>
    Date:   Sat Jun 25 02:16:27 2022 -0500
    
        hero mkb in progress

--- Infantry/main.cpp
+++ Infantry/main.cpp
@@ -1,122 +1,128 @@
-// #include "../src/main.hpp"
-// #include <cstdlib>
+#include "../src/main.hpp"
+#include <cstdlib>
 
-// #define PI 3.14159265
+#define PI 3.14159265
 
-// CANMotor LF(4,NewCANHandler::CANBUS_1,M3508); 
-// CANMotor RF(2,NewCANHandler::CANBUS_1,M3508); 
-// CANMotor LB(1,NewCANHandler::CANBUS_1,M3508); 
-// CANMotor RB(3,NewCANHandler::CANBUS_1,M3508);
-// float multiplier = 3;
+CANMotor LF(4,NewCANHandler::CANBUS_1,M3508); CANMotor RF(2,NewCANHandler::CANBUS_1,M3508); CANMotor LB(1,NewCANHandler::CANBUS_1,M3508); CANMotor RB(3,NewCANHandler::CANBUS_1,M3508);
+float speedmultiplier = 3;
+float powmultiplier = 2;
+float translationalmultiplier = 3;
+float beybladespeedmult = 1;
 
-// float beybladespeedmult = 1;
+CANMotor yaw(5, NewCANHandler::CANBUS_1, GIMBLY);
+CANMotor pitch(6, NewCANHandler::CANBUS_1, GIMBLY);
+int pitchval = 0;
+#define LOWERBOUND 1000
+#define UPPERBOUND 2000
 
-// CANMotor yaw(5, NewCANHandler::CANBUS_1, GIMBLY);
-// CANMotor pitch(6, NewCANHandler::CANBUS_1, GIMBLY);
-// int pitchval = 0;
-// #define LOWERBOUND 1000
-// #define UPPERBOUND 2000
+CANMotor indexer(7, NewCANHandler::CANBUS_1, C610);
+int indexJamTime = 0;
+int lastJam = 0;
 
-// CANMotor indexer(7, NewCANHandler::CANBUS_1, C610);
-// int indexJamTime = 0;
-// int lastJam = 0;
+PWMMotor RFLYWHEEL(D12); PWMMotor LFLYWHEEL(D11);
+PWMMotor flyWheelMotors[] = {RFLYWHEEL, LFLYWHEEL};
 
-// PWMMotor RFLYWHEEL(D12); PWMMotor LFLYWHEEL(D11);
-// PWMMotor flyWheelMotors[] = {RFLYWHEEL, LFLYWHEEL};
+void setFlyWheelPwr(int pwr) {
+    for (int i = 0; i < 2; i++)
+        flyWheelMotors[i].set(pwr);
+}
 
-// void setFlyWheelPwr(int pwr) {
-//     for (int i = 0; i < 2; i++)
-//         flyWheelMotors[i].set(pwr);
-// }
+int main()
+{
+    threadingRemote.start(&remoteThread);
+    //threadingReferee.start(&refereeThread);
+    CANMotor::setCANHandlers(&canHandler1,&canHandler2);
 
-// int main()
-// {
-//     threadingRemote.start(&remoteThread);
-//     //threadingReferee.start(&refereeThread);
-//     CANMotor::setCANHandlers(&canHandler1,&canHandler2);
+    LB.setSpeedPID(1.75, 0.351, 5.63);
+    RF.setSpeedPID(1.073, 0.556, 0);
+    RB.setSpeedPID(1.081, 0.247, 0.386);
+    LF.setSpeedPID(.743, 0.204, 0.284);
+    pitch.setPositionPID(.017,.001,.044);
+    pitch.useAbsEncoder = 1;
+    yaw.setSpeedPID(78.181, 7.303, 1.227);
+    indexer.setSpeedPID(0.014, 0.001, 0.046);
+    indexer.setSpeedIntegralCap(500000);
 
-//     LB.setSpeedPID(1.75, 0.351, 5.63);
-//     RF.setSpeedPID(1.073, 0.556, 0);
-//     RB.setSpeedPID(1.081, 0.247, 0.386);
-//     LF.setSpeedPID(.743, 0.204, 0.284);
-//     pitch.setPositionPID(.017,.001,.044);
-//     pitch.useAbsEncoder = 1;
-//     yaw.setSpeedPID(78.181, 7.303, 1.227);
-//     indexer.setSpeedPID(0.014, 0.001, 0.046);
+    while (true) {
 
-//     while (true) {
-
-//         LF.outCap = 16000;   
-//         RF.outCap = 16000;
-//         LB.outCap = 16000;
-//         RB.outCap = 16000;
+        LF.outCap = 16000;   
+        RF.outCap = 16000;
+        LB.outCap = 16000;
+        RB.outCap = 16000;
         
 
-//         if(rS == 1){ // All non-serializer motors activated
-//             int LFa = lY + lX + Wh, RFa = lY - lX - Wh, LBa = lY - lX + Wh, RBa = lY + lX - Wh;
-//             LF.setSpeed(LFa * multiplier);
-//             RF.setSpeed(-RFa*multiplier);
-//             LB.setSpeed(LBa*multiplier);
-//             RB.setSpeed(-RBa*multiplier);
-            
-//             if (pitchval < LOWERBOUND) // lowerbound
-//                 pitchval = LOWERBOUND;
-//             if (pitchval > UPPERBOUND) //upperbound
-//                 pitchval = UPPERBOUND;
-//             pitchval += (int)myremote.getStickData(RIGHTJOYY, 0, 3);
-//             pitch.setPosition(pitchval);
-//             yaw.setSpeed(rX/100);
+        if(rS == 1){ // All non-serializer motors activated
+            int LFa = lY + lX*translationalmultiplier + rX, RFa = lY - lX*translationalmultiplier - rX, LBa = lY - lX*translationalmultiplier + rX, RBa = lY + lX*translationalmultiplier - rX;
+            // LF.setSpeed(LFa * speedmultiplier);
+            // RF.setSpeed(-RFa* speedmultiplier);
+            // LB.setSpeed(LBa* speedmultiplier);
+            // RB.setSpeed(-RBa* speedmultiplier);
             
 
-//         }else if(rS == 2){ //disable all the non-serializer components
-//             LF.setPower(0);RF.setPower(0);LB.setPower(0);RB.setPower(0);
-//             yaw.setPower(0); pitch.setPower(0);
-//         }else if(rS == 3){ // beyblade mode
-//             double angle = yaw.getData(ANGLE) / 8192.0 * PI * 2;
-//             angle -= PI/4;
+            // if (pitchval < LOWERBOUND) // lowerbound
+            //     pitchval = LOWERBOUND;
+            // if (pitchval > UPPERBOUND) //upperbound
+            //     pitchval = UPPERBOUND;
+            // pitchval += (int)myremote.getStickData(RIGHTJOYY, 0, 3);
+            // pitch.setPosition(pitchval);
+            LF.setPower(LFa * powmultiplier * 1.5);
+            RF.setPower(-RFa * powmultiplier * 1.5);
+            LB.setPower(LBa * powmultiplier);
+            RB.setPower(-RBa * powmultiplier);
             
-//             int beybladespeedmult = 2;
+            pitch.setPower(rY*5);
+            //yaw.setSpeed(rX/100);
+            
 
-//             float raw_x = lX * beybladespeedmult;
-//             float raw_y = lY * beybladespeedmult;
+        }else if(rS == 2){ //disable all the non-serializer components
+            LF.setPower(0);RF.setPower(0);LB.setPower(0);RB.setPower(0);
+            yaw.setPower(0); pitch.setPower(0);
+        }else if(rS == 3){ // beyblade mode
+            // double angle = yaw.getData(ANGLE) / 8192.0 * PI * 2;
+            // angle -= PI/4;
+            
+            // int beybladespeedmult = 2;
 
-//             float x = (float) (raw_x * cos(angle) - raw_y * sin(angle));
-//             float y = (float) (raw_x * sin(angle) + raw_y * cos(angle));
+            // float raw_x = lX * beybladespeedmult;
+            // float raw_y = lY * beybladespeedmult;
 
-//             int beyblade_rotation = 2000;
-//             LF.setSpeed(x + y + beyblade_rotation);
-//             RF.setSpeed(x - y + beyblade_rotation);
-//             LB.setSpeed(-x + y + beyblade_rotation);
-//             RB.setSpeed(-x - y + beyblade_rotation);
-//             yaw.setSpeed(20);
-//         }
+            // float x = (float) (raw_x * cos(angle) - raw_y * sin(angle));
+            // float y = (float) (raw_x * sin(angle) + raw_y * cos(angle));
 
-//         if (lS == 1) {
-//             indexer.setSpeed(rY*5);
-//             setFlyWheelPwr(40);
-//         } else if(lS == 2){ //disable serializer
-//             indexer.setPower(0);
-//             setFlyWheelPwr(0);
-//         }else if(lS == 3){
-//             setFlyWheelPwr(60);
-//                 if(abs(indexer.getData(TORQUE)) > 500 & abs(indexer.getData(VELOCITY)) < 20){ //intial jam detection
-//                     if (lastJam == 0) {
-//                         indexJamTime = us_ticker_read() /1000; // start clock
-//                         lastJam = 1;
-//                         printf("jam detected!\n");
-//                     }
-//                 }
-//                 else 
-//                     lastJam = 0;
+            // int beyblade_rotation = 2000;
+            // LF.setSpeed(x + y + beyblade_rotation);
+            // RF.setSpeed(x - y + beyblade_rotation);
+            // LB.setSpeed(-x + y + beyblade_rotation);
+            // RB.setSpeed(-x - y + beyblade_rotation);
+            // yaw.setSpeed(20);
+        }
+
+        if (lS == 3) {
+            
+        }else if(lS == 2){ //disable serializer
+            indexer.setPower(0);
+            setFlyWheelPwr(0);
+        }else if(lS == 1){
+            setFlyWheelPwr(30);
+                if(abs(indexer.getData(TORQUE)) > 500 & abs(indexer.getData(VELOCITY)) < 20){ //intial jam detection
+                    if (lastJam == 0) {
+                        indexJamTime = us_ticker_read() /1000; // start clock
+                        //indexer.setPower(-7000);
+                        lastJam = 1;
+                        printf("jam detected!\n");
+                    }
+                }
+                else 
+                    lastJam = 0;
                 
-//                 if(lastJam && us_ticker_read() / 1000 - indexJamTime > 75){ // If jam for more than 250ms then reverse
-//                     indexer.setPower(7500); 
-//                     printf("Unjamming...\n");
-//                 }else
-//                     indexer.setSpeed(-50*36); // No Jam, regular state
-//         }
+                if(lastJam && us_ticker_read() / 1000 - indexJamTime > 100){ // If jam for more than 75ms then reverse
+                    indexer.setPower(7500); 
+                    printf("Unjamming...\n");
+                }else
+                    indexer.setSpeed(-75*36); // No Jam, regular state
+        }
 
-//         ThisThread::sleep_for(1ms);
-//     }
-// }
+        ThisThread::sleep_for(1ms);
+    }
+}
 
--- Test Bench/PIDTunerDJIRemote.cpp
+++ Test Bench/PIDTunerDJIRemote.cpp
@@ -1,21 +1,24 @@
 #include "mbed.h"
 #include "../src/main.hpp"
 
-// PWMMotor leftFlywheelTop(PA_5);
-// PWMMotor rightFlywheelTop(PA_6);
-// PWMMotor leftFlywheelBot(PB_6);
-// PWMMotor rightFlywheelBot(PA_7);
+PWMMotor leftFlywheelTop(PA_5);
+PWMMotor rightFlywheelTop(PA_6);
+PWMMotor leftFlywheelBot(PB_6);
+PWMMotor rightFlywheelBot(PA_7);
 
 // Either MULTITURNANGLE or VELOCITY 
 
-#define motorID 2
-#define motorType M3508
-#define MOTORMODE VELOCITY
-#define outputCap 0
-#define integralCap 0
+#define motorID 3
+#define motorType GM6020
+#define MOTORMODE MULTITURNANGLE
+#define outputCap 30000
+#define integralCap 10000
 #define feedForwardVal 0 //useful for overcoming gravity
 
-int posMotorCommands[] = {1000,1500,2000};
+PWMMotor LFLYWHEEL(D11);
+PWMMotor RFLYWHEEL(D12);
+
+int posMotorCommands[] = {4000,5000,6000};
 int speedMotorCommands[] = {0,45*19,90*19};
 
 CANMotor testMotor(motorID, NewCANHandler::CANBUS_1, motorType);
@@ -23,19 +26,19 @@ CANMotor testMotor(motorID, NewCANHandler::CANBUS_1, motorType);
 float PIDvals[] = {0, 0, 0};
 
 void updateTestMotorCommand() {
-    if (myremote.getSwitchData(LSWITCH) == 1) { 
+    if (lS == 1) { 
             if (MOTORMODE == MULTITURNANGLE)
                 testMotor.setPosition(posMotorCommands[0]);
             else 
                 testMotor.setSpeed(speedMotorCommands[0]);
     }
-    if (myremote.getSwitchData(LSWITCH) == 2) {
+    if (lS == 2) {
             if (MOTORMODE == MULTITURNANGLE) 
                 testMotor.setPosition(posMotorCommands[1]);
             else
                 testMotor.setSpeed(speedMotorCommands[1]);
     }
-    if (myremote.getSwitchData(LSWITCH) == 3) {
+    if (lS == 3) {
             if (MOTORMODE == MULTITURNANGLE) 
                 testMotor.setPosition(posMotorCommands[2]);
             else
@@ -45,23 +48,22 @@ void updateTestMotorCommand() {
 
 
 void printInfo() {
-        if (myremote.getSwitchData(RSWITCH) == 1) {
+        if (rS == 1) {
             if (MOTORMODE == MULTITURNANGLE) 
                 printf("Des: %d \t Act: %d", testMotor.getValue(), testMotor.getData(MULTITURNANGLE));
             if (MOTORMODE == VELOCITY)
                 printf("Des: %d \t Act: %d", testMotor.getValue(), testMotor.getData(VELOCITY));
         }
-        else if (myremote.getSwitchData(RSWITCH) == 2) {
+        else if (rS == 2) {
             for (int i = 0; i< 3; i++) {
-                printf("%d", (int)(PIDvals[i]*1000));
+                printf("%d", (int)(PIDvals[i]*10000));
                 //printFloat(PIDvals[i], 3);
                 printf("\t");
             }
             if (MOTORMODE == MULTITURNANGLE) 
-                printf("Des: %d \t Act: %d \t", testMotor.getValue(), testMotor.getData(MULTITURNANGLE));
+                printf("Des: %d \t Act: %d\t Pwr: %d", testMotor.getValue(), testMotor.getData(MULTITURNANGLE), testMotor.getPowerOut());
             if (MOTORMODE == VELOCITY)
-                printf("Des: %d \t Act: %d", testMotor.getValue(), testMotor.getData(VELOCITY));
-            
+                printf("Des: %d \t Act: %d\t", testMotor.getValue(), testMotor.getData(VELOCITY));
             //printf("Pwr: %d", testMotor.getPowerOut());
         }
         else {
@@ -74,6 +76,7 @@ int main(void) {
     CANMotor::setCANHandlers(&canHandler1,&canHandler2);
     threadingRemote.start(remoteThread);
     testMotor.pidPosition.feedForward = feedForwardVal;
+    
 
     if (outputCap != 0)
         testMotor.setPositionOutputCap(outputCap);
@@ -84,16 +87,18 @@ int main(void) {
 
 
     while (1) {
+        testMotor.pidPosition.setOutputCap(30000);
         if (MOTORMODE == MULTITURNANGLE)
             testMotor.setPositionPID(PIDvals[0], PIDvals[1], PIDvals[2]);
         if (MOTORMODE == VELOCITY)
             testMotor.setSpeedPID(PIDvals[0], PIDvals[1], PIDvals[2]);
-
+        ThisThread::sleep_for(1ms);
         updateTestMotorCommand();
+        ThisThread::sleep_for(1ms);
         printInfo();
 
         PIDvals[0] += myremote.getStickData(LEFTJOYX, 0, .01);
-        PIDvals[1] -= myremote.getStickData(WHEEL, 0, 0.001);
+        PIDvals[1] -= myremote.getStickData(WHEEL, 0, 0.0001);
         PIDvals[2] += myremote.getStickData(RIGHTJOYX, 0, .01);
 
         for (int i = 0; i < 3; i++) 
